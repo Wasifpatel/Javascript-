5904410a5040d61b4d4f02c626b41d618bef6abe
Wasifpatel <140307922+Wasifpatel@users.noreply.github.com>
2025-04-24 08:42:36 +0000

    datatype_summary

---
+++ 01_basics/06_datatype_summary.js
@@ -0,0 +1,33 @@
+// primitive
+
+// 7types - Number , String , Boolean , Null , Undefined , Symbol , BigInt
+
+// Non-primitive (Reference)
+
+// Array , Object , Function
+const arr = ["ironman","spiderman","batman"]
+
+let myobj = {
+    name: "wasif",
+    age: 21
+}
+
+const myfunc = function name(params) {
+    console.log("Hello Guys");
+}
+myfunc();
+
+// Return type of variables in JavaScript
+// 1) Primitive Datatypes
+//        Number => number
+//        String  => string
+//        Boolean  => boolean
+//        null  => object
+//        undefined  =>  undefined
+//        Symbol  =>  symbol
+//        BigInt  =>  bigint
+
+// 2) Non-primitive Datatypes
+//        Arrays  =>  object
+//        Function  =>  function
+//        Object  =>  object
