a1cdd229a36cd1d629ae6f318fd32f5394e5833f
Wasifpatel <140307922+Wasifpatel@users.noreply.github.com>
2025-04-25 08:58:35 +0000

    array

---
+++ 02_basics/01_array.js
@@ -0,0 +1,28 @@
+const myArr =[1,2,3,4,"ironman","spiderman"]
+// console.log(myArr);
+
+// const myArr2 = new Array(7,8,9)
+// console.log(myArr2);
+
+// myArr2.push("wasif")
+// console.log(myArr2);
+// myArr2.pop()  //it doesnt take parameter directly pop last element
+// console.log(myArr2);
+// myArr2.unshift("x")  
+// console.log(myArr2);
+// myArr2.shift()  
+// console.log(myArr2);
+
+// const newArr = myArr2.join()
+
+// console.log(myArr2);
+// console.log(newArr);
+
+// ********** slice , splice ****************
+const myn1 = myArr.slice(1,3)
+console.log(myn1);
+console.log("Before" , myArr);
+
+const myn2 = myArr.splice(1,3)
+console.log(myn2);
+console.log("After" , myArr);
