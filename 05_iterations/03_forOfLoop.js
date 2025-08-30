23b374266ebc10385baf0387b4d73732e2054f5d
Wasifpatel <140307922+Wasifpatel@users.noreply.github.com>
2025-08-30 10:48:21 +0000

    maps notes added

--- 05_iterations/03_forOfLoop.js
+++ 05_iterations/03_forOfLoop.js
@@ -5,12 +5,15 @@ const arr = [1,2,3,4,5]
 // }
 
 for (const val of arr) { // it will automatically get the size of array
-    // console.log(val);           
+    console.log(val);           
 }
 
 // MAPS
 // no duplicate values all unique values
-
+//Keys can be any type (object, array, number, string, boolean, etc.) — unlike normal objects which mostly use strings as keys.
+// Maintains the insertion order of keys.
+// Has useful built-in methods like .set(), .get(), .has(), .delete(), and .size.
+// Easier to iterate compared to plain objects.
 const something = new Map()
 something.set("IN" , "India")
 something.set("USA" , "United States of America")
